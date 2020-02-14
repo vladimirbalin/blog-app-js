edf8152dc841e4bec054c6883681ac09b373f2c0
VladimirB <vladimirzq@gmail.com>
2020-02-15 03:56:56 +0600

    finished

--- src/components/favorites.component.js
+++ src/components/favorites.component.js
@@ -1,17 +1,22 @@
 import { Component } from "../core/component";
 import { apiService } from "../services/api.service";
 import {transform} from "../services/transform";
+import { renderPosts } from "../templates/post.template";
 
 export class FavoritesComponent extends Component{
   constructor(id, {loader}) {
     super(id);
     this.loader = loader;
   }
+  init() {
+    this.$el.addEventListener('click', linkClickHandler.bind(this));
+  }
+
   onShow() {
     const favorites = JSON.parse(localStorage.getItem('favorites'));
 
     this.$el.insertAdjacentHTML('afterbegin', renderList(favorites));
-    this.$el.addEventListener('click', linkClickHandler.bind(this));
+
   }
   onHide() {
     this.$el.innerHTML = '';
@@ -34,34 +39,13 @@ async function linkClickHandler(event){
     event.preventDefault();
     this.loader.show();
 
-    const fbData = await apiService.fetchPosts();     //получаем объект постов с бд
-    const fbDataArr = transform.fbDbToArray(fbData);
-    const html = fbDataArr.map(el => {
-      if(event.target.dataset.id === el.id){
-
-          const tag = el.type === 'news' ? `<div class="tag tag-news">Новость</div>`
-            : `<div class="tag tag-note">Заметка</div>`;
-
-          return `
-            <div class="panel">
-              <div class="panel-head">
-                <p class="panel-title">${el.title}</p>
-                ${tag}
-              </div>
-              <div class="panel-body">
-                <p class="multi-line">${el.fulltext}</p>
-              </div>
-              <div class="panel-footer w-panel-footer">
-                <small>${el.date}</small>
-               
-              </div>
-            </div>
-           `;
-
-      }
-    }).join('');
+    const favoritePostObj = await apiService.fetchPostById(event.target.dataset.id);
+    favoritePostObj.id = event.target.dataset.id;
+    const html = renderPosts(favoritePostObj, false);
     this.$el.insertAdjacentHTML('beforeend', html);
     this.loader.hide();
-    event.target.classList.add('hide');
+    event.target.innerHTML = '';
   }
 }
+
+
